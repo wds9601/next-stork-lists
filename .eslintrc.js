05f178a85a26d6e2c20d7dfd4d2300112d0e9d00
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-03-26 12:21:48 -0700

    added formatting .ignore files and updated prop Type rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,39 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es6: true,
-  },
-  parserOptions: { ecmaVersion: 8 },
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'],
-  extends: ['eslint:recommended'],
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parser: '@typescript-eslint/parser',
-      settings: { react: { version: 'detect' } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-      ],
-      rules: {
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'jsx-a11y/anchor-is-valid': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-          },
-        ],
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-      },
-    },
-  ],
+	root: true, // Make sure eslint picks up the config at the root of the directory
+	parserOptions: {
+		ecmaVersion: 2020, // Use the latest ecmascript standard
+		sourceType: 'module', // Allows using import/export statements
+		ecmaFeatures: {
+			jsx: true, // Enable JSX since we're using React
+		},
+	},
+	settings: {
+		react: {
+			version: 'detect', // Automatically detect the react version
+		},
+	},
+	env: {
+		browser: true, // Enables browser globals like window and document
+		amd: true, // Enables require() and define() as global variables as per the amd spec.
+		node: true, // Enables Node.js global variables and Node.js scoping.
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+	],
+	rules: {
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'jsx-a11y/anchor-is-valid': [
+			'error',
+			{
+				components: ['Link'],
+				specialLink: ['hrefLeft', 'hrefRight'],
+				aspects: ['invalidHref', 'preferButton'],
+			},
+		],
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+	},
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,7 @@ module.exports = {
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
-  useTabs: true,
+  useTabs: false,
   jsxBracketSameLine: false,
   bracketSpacing: true,
 }
