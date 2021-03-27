448d1359b370877ced1732e592d7cc0d1e941a1f
William Downey-Schell <Will@Williams-MacBook-Air.local>
2021-03-26 19:47:32 -0700

    removed some filler text from index.js

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,13 @@ export default function Home() {
 
 			<main>
 				<section className=" flex justify-center">
-					<div className="text-center m-20 max-w-lg p-10 bg-blue-200 border-2 rounded">
+					<div className="text-center m-20 max-w-2xl p-10 bg-blue-200 border-2 rounded">
 						<p className="m-5 text-6xl font-bold font-sans">My Next.js App</p>
 						<p className="m-3 text-lg font-sans">
 							w/ <span className="font-bold font-sans">TailwindCSS</span>,
-							<span className="font-bold font-sans">ESLint</span>, and
-							<span className="font-bold font-sans">Prettier</span> out of the
+							<span className="font-bold font-sans"> ESLint</span>, and
+							<span className="font-bold font-sans"> Prettier</span> out of the
 							box.
-						</p>
-						<p className="text-2xl font-sans m-3">...well, better get to it!</p>
-						<p className="text-base">
-							Clean up this index.js file and start coding!
 						</p>
 					</div>
 				</section>
